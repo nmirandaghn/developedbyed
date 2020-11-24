c3c8514cda8402db14fcdb4ca7822fb95e7bd0ff
Nelson Miranda <nmirandaghn@gmail.com>
2020-11-23 22:26:24 -0600

    Started React Player

---
+++ react/chapter_03/react-player/src/App.js
@@ -0,0 +1,13 @@
+import Player from "./components/Player";
+import Song from "./components/Songs";
+
+function App() {
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
