83355ef251f8dbf2d642c76f8c400ef88c3a7963
Nelson Miranda <nmirandaghn@gmail.com>
2020-12-06 17:20:52 -0600

    Refactoring

--- react/chapter_03/react-player/src/components/LibrarySong.js
+++ react/chapter_03/react-player/src/components/LibrarySong.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({ songs, setCurrentSong, song }) => {
+  const songSelected = () => {
+    const selectedSong = songs.filter((state) => state.id === song.id);
+    setCurrentSong(selectedSong[0]);
+  };
   return (
-    <div className="library-song">
+    <div className="library-song" onClick={songSelected}>
       <img alt={song.name} src={song.cover} />
       <div className="song-description">
         <h3>{song.name}</h3>
